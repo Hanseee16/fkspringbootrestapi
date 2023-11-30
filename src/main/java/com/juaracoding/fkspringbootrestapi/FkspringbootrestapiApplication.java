f93cf6ad4877f473975b1e172a7b8d6371fc7a7e
Hanseee16 <kamilfarhan223@gmail.com>
2023-11-30 22:40:13 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/fkspringbootrestapi/FkspringbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.fkspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkspringbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkspringbootrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/fkspringbootrestapi/FkspringbootrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.fkspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkspringbootrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
