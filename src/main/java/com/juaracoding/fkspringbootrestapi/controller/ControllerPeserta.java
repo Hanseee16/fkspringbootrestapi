d0cb15fd6a9de9994c96b8a873fb4d9f61b33f54
Hanseee16 <kamilfarhan223@gmail.com>
2023-12-09 20:18:33 +0700

    JAVA BACKEND BATCH 18 Day 29

--- src/main/java/com/juaracoding/fkspringbootrestapi/controller/ControllerPeserta.java
+++ src/main/java/com/juaracoding/fkspringbootrestapi/controller/ControllerPeserta.java
@@ -12,10 +12,12 @@ Version 1.0
 import com.juaracoding.fkspringbootrestapi.CobaCoba;
 import com.juaracoding.fkspringbootrestapi.model.ModelPeserta;
 import com.juaracoding.fkspringbootrestapi.repo.RepoPeserta;
+import org.bouncycastle.math.raw.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/peserta")
@@ -25,7 +27,7 @@ public class ControllerPeserta {
     @Autowired
     CobaCoba cobaCoba;
 
-    @GetMapping("/v1/getall")
+    @GetMapping("/v1/getAll")
     public List<ModelPeserta> getAllData() {
         List<ModelPeserta> modelPesertaList = repoPeserta.findAll();
         return modelPesertaList;
@@ -35,5 +37,17 @@ public class ControllerPeserta {
     public String postData(@RequestBody ModelPeserta modelPeserta) {
         repoPeserta.save(modelPeserta);
         return modelPeserta.getNama() + " berhasil ditambahkan";
+    }
+
+    @GetMapping("/v1/searchById")
+    public Object searchPesertaById(@RequestParam(value = "id") Long id) {
+        Optional<ModelPeserta> hasil = repoPeserta.findById(id);
+
+        if (hasil.isEmpty()) {
+            return "Data Tidak Ada!";
+        }
+
+        ModelPeserta p = hasil.get();
+        return p;
     }
 }
---
+++ src/main/java/com/juaracoding/fkspringbootrestapi/service/ServicePeserta.java
@@ -0,0 +1,21 @@
+package com.juaracoding.fkspringbootrestapi.service;
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Hanseee a.k.a. Farhan Kamil
+Java Developer
+Created on 09/12/2023 18:46
+@Last Modified 09/12/2023 18:46
+Version 1.0
+*/
+
+import com.juaracoding.fkspringbootrestapi.repo.RepoPeserta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ServicePeserta {
+
+    @Autowired
+    RepoPeserta repoPeserta;
+}
